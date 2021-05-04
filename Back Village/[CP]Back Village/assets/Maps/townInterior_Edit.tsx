d03ae973a2b16c8f1b28fb1447298d9967990b51
Izuku-Midorya <68604999+Izuku-Midorya@users.noreply.github.com>
2021-05-04 18:25:06 +0100

    Add files via upload
    I am looking for people to help me with this task of making a mod similar to ridgeside village. Heres a preview of the files. My Discord ID is Grim Reapers#7929

---
+++ Back Village/[CP]Back Village/assets/Maps/townInterior_Edit.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="townInterior_Edit" tilewidth="16" tileheight="16" tilecount="2304" columns="32">
+ <image source="townInterior_Edit.png" width="512" height="1160"/>
+</tileset>
